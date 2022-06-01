86847c8a5bcf3f5a778619161d5ad9b11fea07e6
EugeneFelix <ontharros@gmail.com>
2022-06-01 18:10:41 +0530

    commit

--- files/blocks.rb
+++ files/blocks.rb
@@ -1,10 +1,18 @@
-ef test
+def test
   yield 5
   puts "You are in the method test"
   yield 100
 end
-test {|i| puts "You are in the block #{i}"}
+
+test {
+  |i| puts "You are in the block #{i}"
+}
+
+
 def test2(&block)
   block.call
 end
-test2 { puts "Hello World!"}
+
+test2 {
+  puts "Hello World!"
+}
