c86091897fd4d4bbf2fa900dbe6383c775ad06c0
EugeneFelix <ontharros@gmail.com>
2022-06-02 23:54:00 +0530

    commit

---
+++ files/linSerach.rb
@@ -0,0 +1,16 @@
+def linear_search(array, key)
+  array.each_with_index do |current, index|
+    return index if current == key
+  end
+  nil
+end
+
+puts "Enter a space-separated list:"
+arr = gets.chomp.split(' ').map(&:to_i)
+puts "Enter a value to be searched:"
+key = gets.chomp.to_i
+puts if linear_search(arr, key) != nil
+  "Found at index #{linear_search(arr, key)}"
+else
+  "Not found"
+end
