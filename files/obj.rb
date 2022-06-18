cb40e6eecdec8180ced29df1d184f71a6b4fca28
EugeneFelix <ontharros@gmail.com>
2022-06-18 14:35:19 +0530

    commit

--- files/obj.rb
+++ files/obj.rb
@@ -1,10 +1,13 @@
 class Bonjour
-  attr_accessor :name
+  attr_accessor :name, :age
+  def initialize(name, age)
+    @name = name
+    @age = age
+  end
   def sayHello
-    "Hello #{@name}"
+    "Hello #{@name} of #{@age} years"
   end
 end
 
-bon = Bonjour.new
-bon.name = "Eugene"
+bon = Bonjour.new "Eugene", "28"
 p bon.name
