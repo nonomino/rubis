9c06742033e3ca834c809c1009a9c4c26bc7ca45
EugeneFelix <ontharros@gmail.com>
2022-07-07 18:55:19 +0530

    commit

---
+++ files/select.rb
@@ -0,0 +1,15 @@
+def selSort array
+  #n = array.length - 1
+  (array.length - 1).times do | i |
+    idx = i
+    for j in (i + 1)..(array.length - 1)
+      idx = j if array[j] < array[idx]
+    end
+    array[i], array[idx] = array[idx], array[i] if idx != i
+  end
+  p array
+end
+
+hello = [1,6,8,4,2,3]
+selSort hello
+
