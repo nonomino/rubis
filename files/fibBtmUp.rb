fc8710fa328f9d1123b7a54577a1d669697a5b83
EugeneFelix <ontharros@gmail.com>
2022-08-14 11:34:31 +0530

    commit

---
+++ files/fibBtmUp.rb
@@ -0,0 +1,13 @@
+def fibbup(n)
+  return 1 if n == 1 or n == 2
+  b = Array.new
+  b[0] = 0
+  b[1] = 1
+  b[2] = 1
+  for i in 2..n do
+    b[i] = b[i - 1] + b[i - 2]
+  end
+  b
+end
+
+p fibbup 55
