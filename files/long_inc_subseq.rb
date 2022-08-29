1094af317a4348cf504c42da40a626504699fef6
EugeneFelix <ontharros@gmail.com>
2022-08-29 21:09:57 +0530

    commit

---
+++ files/long_inc_subseq.rb
@@ -0,0 +1,17 @@
+def longest_incr_subseq(seq)
+  l = {}
+
+  seq.length.times do |j|
+    l_conn_keys = l.keys.select do |i|
+      i < j and seq[i] < seq[j]
+    end
+
+    l_conn_values = l_conn_keys.map do |k|
+      l[k]
+    end
+
+    l[j] = 1 + (l_conn_values.max || 0)
+  end
+
+  return l.values.max
+end
