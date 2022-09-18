2a3a7b29d9dcac394fde5f0d294a5c8c11a95ef6
EugeneFelix <ontharros@gmail.com>
2022-09-18 21:34:20 +0530

    commit

--- files/thor.rb
+++ files/thor.rb
@@ -5,7 +5,7 @@ require 'thor'
 class CoolCLI < Thor
   desc "hello lang", "say hello in given language"
   def hello(name)
-    puts "Bonjour #{name}"
+    puts "Bonjour Monsieur #{name}"
   end
 end
 
