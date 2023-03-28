1b1dfebd461748749e6c1adbaaf1c0445977b1a7
EugeneFelix <ontharros@gmail.com>
2023-03-28 07:24:58 +0530

    commit

--- files/sqlt.rb
+++ files/sqlt.rb
@@ -5,6 +5,8 @@ require "sqlite3"
 begin
   db = SQLite3::Database.open 'test.db'
   db.results_as_hash = true
+  sql = db.prepare ""
+  sql.execute
 rescue SQLite3::Exception => e
   puts "Error! Refactor!!"
 ensure
