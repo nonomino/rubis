d404c5f9b6a620efc6abff5b17bedb32478e6a40
EugeneFelix <ontharros@gmail.com>
2023-04-22 22:12:06 +0530

    commit

--- files/thor.rb
+++ files/thor.rb
@@ -6,8 +6,8 @@ class CoolCLI < Thor
   desc "hello lang", "say hello in given language"
   def hello(name)
     puts "Bonjour Monsieur #{name}"
+    puts "Hola Maestro #{name}"
   end
 end
 
 CoolCLI.start(ARGV)
-exec "ruby -v"
