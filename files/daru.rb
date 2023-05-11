6b0d3a53e8999d985d7a981d8ab92ba512f77f17
EugeneFelix <ontharros@gmail.com>
2023-05-11 23:01:09 +0530

    commit

--- files/daru.rb
+++ files/daru.rb
@@ -2,5 +2,5 @@
 
 require 'daru'
 vector = Daru::Vector.new([20,40,25,50,45,12], index: ['cherry', 'apple', 'barley', 'wheat', 'rice', 'sugar'], 
-  name: "Prices of stuff.")
+  name: "Prices of food.")
 p vector
