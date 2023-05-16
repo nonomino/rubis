7e3fdd4fb0f9026752bd71b2c32ddc0935f2a33c
EugeneFelix <ontharros@gmail.com>
2023-05-16 19:31:33 +0530

    commit

--- files/ll.rb
+++ files/ll.rb
@@ -8,7 +8,10 @@ end
 n1, n2 = Node.new, Node.new
 n1.value = 15
 n2.value = 30
-n1.next = n2
-n2.next = nil
-p n1
-p n2  
+n2.next = n1
+
+node = n2
+while(node) do
+  puts node.value
+  node = node.next
+end
