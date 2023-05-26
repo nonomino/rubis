327f35ca89228f5bf0c71036a2258bc4eb2598f6
EugeneFelix <ontharros@gmail.com>
2023-05-26 22:09:45 +0530

    commit

--- files/powerSet.rb
+++ files/powerSet.rb
@@ -2,7 +2,8 @@
 
 def get_numbers(list, index = 0, taken = [])
   return [taken] if index == list.size
-  get_numbers(list, index+1, taken) +
-  get_numbers(list, index+1, taken + [list[index]])
+  # recurse over get_numbers
+  get_numbers(list, index+1, taken) + get_numbers(list, index+1, taken + [list[index]])
 end
+
 p get_numbers([1,2,3])
