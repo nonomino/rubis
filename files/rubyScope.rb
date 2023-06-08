b3a80280269561bd56d21578333be04533332e12
EugeneFelix <ontharros@gmail.com>
2023-06-08 22:52:55 +0530

    commit

--- files/rubyScope.rb
+++ files/rubyScope.rb
@@ -7,12 +7,14 @@ class Inside_one
       ::CONST + ' inside one'
    end
 end
+
 class Inside_two
    CONST = ' inside two'
    def where_is_my_CONST
       CONST
    end
 end
+
 puts Inside_one.new.where_is_my_CONST
 puts Inside_two.new.where_is_my_CONST
 puts Object::CONST + Inside_two::CONST
