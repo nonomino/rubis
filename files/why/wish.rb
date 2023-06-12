9d305c7e8a7bb8753fcf1f4293b8cf78481db949
EugeneFelix <ontharros@gmail.com>
2023-06-12 08:37:26 +0530

    commit

--- files/why/wish.rb
+++ files/why/wish.rb
@@ -1,8 +1,19 @@
+include 'endertromb'
+
 class WishMaker
   def initialize
     @energy = rand(5)
   end
+
   def grant( wish )
     raise ArgumengError, "No More energy" if wish.length > 10
   end
+
+  if @energy.zero?
+    raise Exception, "No energy left."
+  end
+  @energy -= 1
+
+  Endertromb::make( wish )
+
 end
