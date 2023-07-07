f05c9716a0717b7683de9ace366af4d530ad58c4
EugeneFelix <ontharros@gmail.com>
2023-07-07 17:50:14 +0530

    commit

--- files/server.rb
+++ files/server.rb
@@ -2,9 +2,9 @@
 
 require 'socket'
 
-server = TCPServer.new('localhost' , 5757);
+server = TCPServer.new('localhost' , 5555);
 loop {
     client = server.accept;
     request = client.readpartial(2048);   
-    puts request
+    pp request
 }
